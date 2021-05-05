b0d6950caf3068625c492dca72b1d40145fe79c2
Matt Antone <matt.antone@gmail.com>
2021-05-05 16:31:47 -0700

    udpates

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
 import Head from 'next/head'
-import Header from '@components/Header'
 import Footer from '@components/Footer'
-
+import Hero from '@components/Hero'
+import Header from '@components/Header'
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
+      <Header/>
+      <Hero/>
+      <div className="container mx-auto">
+        <main>
+          <p className="description">
+            Get started by editing <code>pages/index.js</code>
+          </p>
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   )
 }
